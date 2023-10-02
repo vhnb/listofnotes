c866ee0bd6bfc3cec908fc728da6f49148d1796a
Victor Henrique <123006884+victorhnbas@users.noreply.github.com>
2023-10-02 20:49:35 -0300

    Add files via upload

---
+++ assets/script/script.js
@@ -0,0 +1,33 @@
+const btnAction = document.getElementById("btn");
+
+btnAction.addEventListener("click", function() {
+
+    const textBox = document.getElementById("input").value;
+    const containerList = document.createElement("div");
+    containerList.className = "card-notas";
+    const textElement = document.createElement("p");
+    textElement.textContent = textBox;
+
+    const btnRemove = document.createElement("button");
+    btnRemove.innerHTML = `<i class="fa-solid fa-trash"></i>`;
+    btnRemove.className = "btnRemove";
+    
+    btnRemove.addEventListener("click", function(){
+        containerList.remove();
+    });
+
+    const btnEdit = document.createElement("button");
+    btnEdit.innerHTML = `<i class="fa-solid fa-pen-to-square"></i>`;
+    btnEdit.className = "btnEdit";
+
+    btnEdit.addEventListener("click", function(){
+        const alterar = prompt("Alterar tarefa!");
+            textElement.textContent = alterar;
+    });
+
+    containerList.appendChild(textElement);
+    containerList.appendChild(btnRemove);
+    containerList.appendChild(btnEdit);
+
+    document.getElementById("container-notas").appendChild(containerList);
+});
