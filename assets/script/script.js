1d1f9e86e4dea6e4b0451fd6d9be50ca6dc5ea9a
Victor Henrique <123006884+victorhnbas@users.noreply.github.com>
2023-11-12 21:09:00 -0300

    Update script.js

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,11 +1,15 @@
 const btnAction = document.getElementById("btn");
 
-btnAction.addEventListener("click", function() {
-    document.getElementById("saida").innerHTML = ''
+document.addEventListener("DOMContentLoaded", function () {
+    loadTasks();
+});
+
+btnAction.addEventListener("click", function () {
+    document.getElementById("saida").innerHTML = '';
     const textBox = document.getElementById("input").value;
 
-    if(!textBox) {
-        document.getElementById("saida").innerHTML = "Insira algo no campo"
+    if (!textBox) {
+        document.getElementById("saida").innerHTML = "Insira algo no campo";
         return;
     }
 
@@ -17,18 +21,25 @@ btnAction.addEventListener("click", function() {
     const btnRemove = document.createElement("button");
     btnRemove.innerHTML = `<i class="fa-solid fa-trash"></i>`;
     btnRemove.className = "btnRemove";
-    
-    btnRemove.addEventListener("click", function(){
+
+    btnRemove.addEventListener("click", function () {
         containerList.remove();
+        saveTasks(); 
     });
 
     const btnEdit = document.createElement("button");
     btnEdit.innerHTML = `<i class="fa-solid fa-pen-to-square"></i>`;
     btnEdit.className = "btnEdit";
 
-    btnEdit.addEventListener("click", function(){
+    btnEdit.addEventListener("click", function () {
         const alterar = prompt("Alterar tarefa!");
+        if(!alterar){
+            return;
+        }else{
             textElement.textContent = alterar;
+            saveTasks();
+        }
+        
     });
 
     containerList.appendChild(textElement);
@@ -36,4 +47,53 @@ btnAction.addEventListener("click", function() {
     containerList.appendChild(btnEdit);
 
     document.getElementById("container-notas").appendChild(containerList);
+
+    saveTasks();
 });
+
+function loadTasks() {
+    const savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+    savedTasks.forEach(function (taskText) {
+        const containerList = document.createElement("div");
+        containerList.className = "card-notas";
+        const textElement = document.createElement("p");
+        textElement.textContent = taskText;
+
+        const btnRemove = document.createElement("button");
+        btnRemove.innerHTML = `<i class="fa-solid fa-trash"></i>`;
+        btnRemove.className = "btnRemove";
+
+        btnRemove.addEventListener("click", function () {
+            containerList.remove();
+            saveTasks();
+        });
+
+        const btnEdit = document.createElement("button");
+        btnEdit.innerHTML = `<i class="fa-solid fa-pen-to-square"></i>`;
+        btnEdit.className = "btnEdit";
+
+        btnEdit.addEventListener("click", function () {
+        const alterar = prompt("Alterar tarefa!");
+        if(!alterar){
+            return;
+        }else{
+            textElement.textContent = alterar;
+        }
+        saveTasks();
+        
+    });
+
+        containerList.appendChild(textElement);
+        containerList.appendChild(btnRemove);
+        containerList.appendChild(btnEdit);
+
+        document.getElementById("container-notas").appendChild(containerList);
+    });
+}
+
+function saveTasks() {
+    const taskElements = document.querySelectorAll(".card-notas p");
+    const tasks = Array.from(taskElements).map((element) => element.textContent);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
